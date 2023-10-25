d47f02b247406cfeed185aa67444a9f045752adc
Ewenn V <146433402+EwennV@users.noreply.github.com>
2023-10-25 23:11:32 +0200

    Ajout des caches dans gitignore

--- cube1/urls.py
+++ cube1/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import API
+import rest_framework
+from django.conf.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', API)
+    path('api-auth/', include('rest_framework.urls'))
 ]
 
