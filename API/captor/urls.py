0201c5d406f1b64da8ecf8bf7ba956bd9c725756
Ewenn V <146433402+EwennV@users.noreply.github.com>
2023-10-26 00:21:27 +0200

    ajout de la route create pour les capteurs

--- API/captor/urls.py
+++ API/captor/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('list', views.list),
+    path('create', views.create),
     path('update', views.update)
 ]
--- API/captor/views.py
+++ API/captor/views.py
@@ -11,6 +11,14 @@ def list(request):
     }
     return JsonResponse(data, status=200)
 
+def create(request):
+    data = {
+        'name': 'John Doe',
+        'age': 30,
+        'email': 'john.doe@example.com'
+    }
+    return JsonResponse(data, status=200)
+
 def update(request):
     data = {
         'name': 'John Doe',
