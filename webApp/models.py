a9507eb65c3d9034cdbb7a50e262317169045463
Ewenn V <146433402+EwennV@users.noreply.github.com>
2023-10-25 08:56:24 +0200

    Modification du nom des objets

--- webApp/models.py
+++ webApp/models.py
@@ -1,20 +1,20 @@
 import uuid
 from django.db import models
 
-class capteur(models.Model):
+class Captor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    name_capteur = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
     lattitudde = models.FloatField(null=True)
     longitude = models.FloatField(null=True)
 
     def __str__(self) -> str:
         return id
 
-class survey(models.Model):
+class Survey(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     temperature = models.FloatField(null=True)
     humidity = models.IntegerField(null=True)
     battery_level = models.FloatField()
     rssi = models.IntegerField()
     date = models.DateTimeField(auto_now_add=True)
-    capteur = models.ForeignKey(capteur, null=True, on_delete=models.SET_NULL)
+    capteur = models.ForeignKey(Captor, null=True, on_delete=models.SET_NULL)
