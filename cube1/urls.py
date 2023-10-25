a5342d6b8f07d8264eb6d7cee50c48294e07c4a1
Ewenn V <146433402+EwennV@users.noreply.github.com>
2023-10-25 16:49:41 +0200

    Ajout du package djangorestframework

--- cube1/urls.py
+++ cube1/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import API
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', API)
 ]
+
