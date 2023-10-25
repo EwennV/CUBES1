7d4f800a3345d88a3bcacb2d0d6198c06697bb3b
Ewenn V <146433402+EwennV@users.noreply.github.com>
2023-10-25 23:42:07 +0200

    Création des templates des routes de l'API

--- cube1/urls.py
+++ cube1/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import rest_framework
-from django.conf.urls import include
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api/', include('API.urls'))
 ]
 
