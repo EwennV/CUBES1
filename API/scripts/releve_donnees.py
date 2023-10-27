6d6733d384a915c36fb50b57f889c6f49064ef2f
Ewenn V <146433402+EwennV@users.noreply.github.com>
2023-10-27 14:39:05 +0200

    add readme

--- API/scripts/releve_donnees.py
+++ API/scripts/releve_donnees.py
@@ -4,11 +4,20 @@ from datetime import datetime
 from API import models
 import requests
 
+from pathlib import Path
+import os
+import environ
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 class ExampleThread(Thread):
     def run(self):
         while(True):
             time.sleep(1)
-            response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+            response = requests.get(f'http://app.objco.com:8099/?account=${env("API_KEY")}&limit=6')
             btc = response.json()
             print(btc['bpi']['USD']['rate'])
 
