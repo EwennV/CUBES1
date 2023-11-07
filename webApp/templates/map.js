dbd93824f0bbe35cb7dfc0be86adaf08b9923c2d
GYSELINGS Terry <clemter@chu-bordeaux.fr>
2023-11-07 13:25:17 +0100

    boxing template + map

---
+++ webApp/templates/map.js
@@ -0,0 +1,19 @@
+function init () {
+    const CESI = {
+        lat: 44.865154,
+        lng: -0.577127
+    }
+
+    const zoom = 17
+
+    const map = L.map('map').setView([CESI.lat, CESI.lng], zoom)
+    
+    const mainLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    mainLayer.addTo(map);
+}
+
+init()
