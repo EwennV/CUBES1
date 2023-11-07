51d7d9af4bac2aefb13316ca39d2a07c3a292408
fromeof <rfialtreaufeigne@gmail.com>
2023-11-07 20:51:28 +0100

    Ajout de la boucle pour l'affichage des données

--- webApp/urls.py
+++ webApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
+from .views import historique
 
 urlpatterns = [
-    path('home/', views.home)
+    path('home/', views.home),
+    path('historique/', historique, name='historique')
 ]
--- webApp/views.py
+++ webApp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here
 
 def home(request):
     return render(request, 'sensors/list.html')
+
+
+def historique(request):
+   
+    response = requests.get('/api/survey/list')
+    data = response.json()  
+
+    return render(request, 'base.html', {'data': data})
