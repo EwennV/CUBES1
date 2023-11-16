adf89895c7f3aa6cc964ee82a82a2fc6d573b7ca
EwennV <ewenn.vallois.perso@gmail.com>
2023-11-16 20:04:28 +0100

    Ajout des modifications de Romeo

--- webApp/views.py
+++ webApp/views.py
@@ -9,6 +9,17 @@ def home(request):
 def historique(request): 
     requests.get('/api/survey/list')
     data = [
-        {'sensor_id': 'name', 'date': 'date': 'temperature':'temperature', 'humidity':'humidity'}
     ]
     return render(request, 'historique.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def alerte(request):
+    return render(request, 'alerte.html')
+
+def carte(request):
+    return render(request, 'carte.html')
+
+def detail(request):
+    return render(request, 'detail.html')
