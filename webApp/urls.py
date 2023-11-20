e6d0119051e04a02ce2172800d6e46dc09b7cd5a
Terry GYSELINGS <terry@MacBook-Air-de-Terry.local>
2023-11-20 16:14:15 +0100

    Miam

--- webApp/urls.py
+++ webApp/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path, include
 from . import views
-from .views import historique
-from .views import dashboard
-from .views import alerte
-from .views import carte
-from .views import detail
 
 urlpatterns = [
-    path('home/', views.home),
-    path('historique/', historique, name='historique'),
-    path('dashboard/', dashboard, name='dashboard'),
-    path('alerte/', alerte, name='alerte'),
-    path('carte/', carte, name='carte'),
-    path('detail/', detail, name='detail')
+    path('', views.dashboard, name='dashboard'),
+    path('historique/', views.historique, name='historique'),
+    path('alerte/', views.alerte, name='alerte'),
+    path('carte/', views.carte, name='carte'),
+    path('detail/', views.detail, name='detail')
 ]
