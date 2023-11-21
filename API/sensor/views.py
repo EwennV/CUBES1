256ca1fb0cd821afe5efa846a0450847b7b0e6a2
Ewenn V <146433402+EwennV@users.noreply.github.com>
2023-11-21 22:52:05 +0100

    Feature/#36 requete create

--- API/sensor/views.py
+++ API/sensor/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.core import serializers  
 from API import models
 from API.scripts import error_response
