7c0b5ca75eb5ee5910d3753ff83a09f93c67f4a7
Terry GYSELINGS <terry@MacBook-Air-de-Terry.local>
2023-11-21 14:03:48 +0100

    modification dashboard

--- webApp/views.py
+++ webApp/views.py
@@ -11,7 +11,23 @@ def historique(request):
     return render(request, 'historique.html', data)
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    r = requests.get('http://localhost:8000/api/sensor')
+
+    data = {
+        'data': []
+    }
+
+    for sensor in r.json():
+        r2 = requests.get(f'http://localhost:8000/api/survey/list?id={sensor["pk"]}&limit=1')
+        r2 = r2.json()[0]
+        data['data'].append({
+            'sensor_id': sensor["pk"],
+            'humidity': r2["fields"]["humidity"],
+            'temperature': r2["fields"]["temperature"],
+            'battery_level': r2["fields"]["battery_level"],
+            'rssi': r2["fields"]["rssi"]
+        })
+    return render(request, 'dashboard.html', data)
 
 def alerte(request):
     return render(request, 'alerte.html')
@@ -20,4 +36,18 @@ def carte(request):
     return render(request, 'carte.html')
 
 def detail(request):
-    return render(request, 'detail.html')
+    r = requests.get('http://localhost:8000/api/sensor')
+
+    data = {
+        'data': []
+    }
+
+    for sensor in r.json():
+        r2 = requests.get(f'http://localhost:8000/api/survey/list?id={sensor["pk"]}&limit=1')
+        r2 = r2.json()[0]
+        data['data'].append({
+            'sensor_id': sensor["pk"],
+            'humidity': r2["fields"]["humidity"],
+            'temperature': r2["fields"]["temperature"],
+        })
+    return render(request, 'detail.html', data)
