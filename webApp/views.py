56232b1b56002ca2fc53a22b25ce93bdb85ce2bf
Ewenn V <146433402+EwennV@users.noreply.github.com>
2023-11-21 11:46:28 +0100

    Ajout de la liste des capteurs dans le dashboard

--- webApp/views.py
+++ webApp/views.py
@@ -12,7 +12,24 @@ def historique(request):
     return render(request, 'historique.html', context)
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    r = requests.get('http://localhost:8000/api/sensor')
+
+    truc = {
+        'data': []
+    }
+
+    for sensor in r.json():
+        r2 = requests.get(f'http://localhost:8000/api/survey/list?id={sensor["pk"]}&limit=1')
+        r2 = r2.json()[0]
+        truc['data'].append({
+            'sensor_id': sensor["pk"],
+            'humidity': r2["fields"]["humidity"],
+            'temperature': r2["fields"]["temperature"],
+            'battery_level': r2["fields"]["battery_level"],
+            'rssi': r2["fields"]["rssi"]
+        })
+    print(truc)
+    return render(request, 'dashboard.html', truc)
 
 def alerte(request):
     return render(request, 'alerte.html')
