fa56038020d248d8186496d789ee6876b975d1e9
fromeof <135048491+fromeof@users.noreply.github.com>
2023-11-22 10:11:09 +0100

    Feature/#33 style  (#9)
    * style.css historique
    
    * supression d'éléments inutiles
    
    * Ajout de la charte humidity
    
    * Ajout du graph temp + modifs couleurs

--- webApp/static/js/script.js
+++ webApp/static/js/script.js
@@ -1,6 +1,5 @@
-const ctx = document.getElementsByClassName('myChart');
-      
-new Chart(ctx, {
+const barCanvas = document.getElementById('barCanvas');
+const barChart = new Chart(barCanvas, {
     type: 'bar',
     data: {
         labels: ['date', 'date', 'date', 'date', 'date', 'date'],
@@ -17,4 +16,25 @@ new Chart(ctx, {
             }
         }
         }
+});
+
+const tempCanvas = document.getElementById('tempCanvas');
+const tempChart = new Chart(tempCanvas, {
+    type: 'line',  
+    data: {
+        labels: ['date', 'date', 'date', 'date', 'date', 'date'],
+        datasets: [{
+            label: 'température',
+            data: [22, 19, 23, 27, 24, 21],
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
 });
