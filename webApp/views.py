71062bbafc1fecc8a99b74277982d84e72c9aaf8
terry <terry.gyselings@viacesi.fr>
2023-11-22 17:33:16 +0100

    ajout des graphiques

--- webApp/views.py
+++ webApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 import requests
 # Create your views here
 
@@ -48,22 +49,44 @@ def carte(request):
     return render(request, 'carte.html')
 
 def detail(request, sensorId):
-    r = requests.get('http://localhost:8000/api/sensor')
-
     data = {
-        'data': []
     }
-    # print(sensorId)
-    # print(r.json())
+
     r2 = requests.get(f'http://localhost:8000/api/survey/list?id={sensorId}&limit=1')
     print(f'http://localhost:8000/api/survey/list?id={sensorId}&limit=1')
     r2 = r2.json()[0]
 
-    data['data'].append({
+    data['data']= {
         'sensor_id': sensorId,
         'humidity': r2["fields"]["humidity"],
         'temperature': r2["fields"]["temperature"],
         'battery_level': r2["fields"]["battery_level"],
         'rssi': r2["fields"]["rssi"]
-    })
+    }
+    
+    graph_data = {
+        'labels': [],  # Liste pour les dates
+        'clean_date': [],
+        'temperature_data': [],  # Liste pour les températures
+        'humidity_data': []  # Liste pour les humidités
+    }
+
+    r3 = requests.get(f'http://localhost:8000/api/survey/list?id={sensorId}&limit=500')
+
+    surveys = r3.json()
+
+    for survey in surveys:
+        graph_data['labels'].append(survey["fields"]["date"])
+        datetime_object = datetime.strptime(survey["fields"]["date"], '%Y-%m-%dT%H:%M:%SZ')
+        heure = datetime_object.strftime('%H:%M')
+        graph_data['clean_date'].append(heure)
+        graph_data['temperature_data'].append(survey["fields"]["temperature"])
+        print(survey["fields"]["temperature"])
+        graph_data['humidity_data'].append(survey["fields"]["humidity"])
+
+    data['graph_data'] = graph_data
+
     return render(request, 'detail.html', data)
+
+
+    
