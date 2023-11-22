a31f42647302dd4656e6f62659f2fee81c7f0d01
fromeof <rfilatreaufeigne@gmail.com>
2023-11-22 09:53:08 +0100

    Ajout du graph temp + modifs couleurs

--- webApp/static/js/script.js
+++ webApp/static/js/script.js
@@ -17,3 +17,24 @@ const barChart = new Chart(barCanvas, {
         }
         }
 });
+
+const tempCanvas = document.getElementById('tempCanvas');
+const tempChart = new Chart(tempCanvas, {
+    type: 'line',  
+    data: {
+        labels: ['date', 'date', 'date', 'date', 'date', 'date'],
+        datasets: [{
+            label: 'température',
+            data: [22, 19, 23, 27, 24, 21],
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
