bcf429352b3ef26b88834589cdc792c7e48027bf
TerryGyselings <148775137+TerryGyselings@users.noreply.github.com>
2023-11-29 14:30:56 +0100

    Terry (#56)
    * correction start zero
    
    * modification affichage date
    
    * changement id pour nom_capteur
    
    * debut tagify
    
    * modification historique
    
    * ajout page modification
    
    * initialisation page modification
    
    * ajout page ajout
    
    * request post ajout
    
    * ajout page modification
    
    * modification nom dashboard
    
    * ajout fonction delete
    
    * ordre historique par date
    
    * bouton delete qui marche pas
    
    * modification de sonde ok
    
    * suppresion sonde ok

--- webApp/static/js/delete.js
+++ webApp/static/js/delete.js
@@ -1,13 +1,26 @@
-document.getElementById('deleteButton').addEventListener('onclick', function() {
-    var sensorId = document.getElementById('inputId');
+function deleteSensor() {
+    const idValue = document.getElementById('idInput').getAttribute('data-sensor-id');
 
-    // Effectuez la requête vers le backend Django
-    fetch('/api/sensor/delete/?sensor_id=' + sensorId)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.message);  // Faites quelque chose avec la réponse du serveur
-        })
-        .catch(error => {
-            console.error('Erreur lors de la suppression du capteur:', error);
-        });
-});
+    fetch(`http://localhost:8000/api/sensor/delete?id=${idValue}`, {
+    method: "DELETE",
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+    })
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data); // Faites quelque chose avec les données renvoyées par le serveur
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+document.getElementById('deleteButton').addEventListener('click',function(event) {
+    event.preventDefault();
+    deleteSensor();
+})
